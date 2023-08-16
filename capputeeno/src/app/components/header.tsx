358ce2d9ebf2830ac8e9f2a1756b3425e38a0742
Guilherme <guilherme.frias.o.t3@gmail.com>
2023-08-16 00:46:35 -0300

    [Feat]: Header stylization complete

--- capputeeno/src/app/components/header.tsx
+++ capputeeno/src/app/components/header.tsx
@@ -1,7 +1,10 @@
+"use client"
 import styled from "styled-components"
-import { Saira_Stencil_One } from "next/font/google";
+import CartIcon from "./icons/cart-icon"
+import SearchIcon from "./icons/search-icon"
+import { Saira_Stencil_One } from "next/font/google"
 
-const sairaStencil = Saira_Stencil_One({
+const sairaStencilOne = Saira_Stencil_One({
     weight: ['400'],
     subsets: ['latin']
 })
@@ -9,18 +12,56 @@ const sairaStencil = Saira_Stencil_One({
 export default function Header() {
     
     const Header = styled.header`
-        
+        background-color: var(--bg-header);
+        width: 100vw;
+        min-height: 10vh;
+        max-height: 20.2vh;
+        padding: 1.9rem 16rem;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     `
 
-    const Logo = styled.a`
+    const Logo = styled.h1`
         color: var(--logo-color);
-        font-weigth: 400;
-        font-size: 1.5rem
+        font-size: 4rem;
+        text-decoration: none;
+    `
+
+    const SearchBar = styled.div`
+        width: 35rem;
+        height: 4rem;
+        padding: 1rem 1.6rem;
+        background-color: var(--bg-secondary);
+        border-radius: 8px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    `
+    const SearchBarText = styled.p`
+        font-size: 1.4rem;
+        color: var(--text-dark);
+    `
+    const Nav = styled.nav`
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 2.4rem;
     `
 
     return (
-        <header>
-            <Logo >Capputeeno</Logo>
-        </header>
+        <Header>
+            <Logo className={sairaStencilOne.className}>Capputeeno</Logo>
+            <Nav>
+                <SearchBar>
+                    <SearchBarText>Procurando por algo específico?</SearchBarText>
+                    <SearchIcon/>
+                </SearchBar>
+                <CartIcon/>
+            </Nav>
+        </Header>
     );
 }
--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from './components/header'
+
 export default function Home() {
   return (
     <main>
-      <p>Hello there</p>
+      <Header></Header>
     </main>
   )
 }
